187b98356fea5855fdd8bdb79c527c8320c5459a
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2016-02-15 16:51:22 -0330

    Initial commit of website content + build

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 ==================================================
 
 This is the source for [rxbroadcast.website](https://rxbroadcast.website), the website for the [RxBroadcast](https://github.com/whymarrh/RxBroadcast) project.
+
+Quick start
+-----------
+
+To build the site:
+
+    vagrant up
+    vagrant ssh --command 'cd workspace && npm i && npm run build'
---
+++ metalsmith.js
@@ -0,0 +1,33 @@
+const Metalsmith = require("metalsmith");
+const superstatic = require("superstatic").server;
+const watch = require("metalsmith-watch");
+
+const metalsmith = new Metalsmith(__dirname);
+const server = superstatic({
+    port: 8000,
+    cwd: __dirname,
+    config: {
+      public: "./build"
+    }
+});
+
+metalsmith.source("website")
+    .use(watch({
+        log: _ => { }
+    }))
+    .build((err, files) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        server.listen(e => {
+            if (e) {
+                console.error("Server error");
+                console.error(e);
+                return;
+            }
+
+            console.log("Server started on port 8000");
+        });
+    });
